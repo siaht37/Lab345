18508e1384bf2a081e357332fb5ce25b620c5634
Asus <thaihoang03072002@gmail.com>
2023-05-24 16:01:22 +0700

    buoi4

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
 import com.example.demo.services.BookService;
+import com.example.demo.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,11 +17,64 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
     public String showAllBooks(Model model) {
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books", books);
         return "book/list";
+    }
+
+    @GetMapping("/add")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "book/add";
+    }
+
+    //    @PostMapping("/add")
+//    public String addBook(@ModelAttribute("book") Book book){
+//        bookService.addBook(book);
+//        return "redirect:/books";
+//    }
+    @PostMapping("/add")
+    public String addBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult, Model model) {
+        //Trường hợp có lỗi ràng buộc thì trả lại view add
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("book", new Book());
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
+        bookService.addBook(book);
+        return"redirect:/books";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        Book book = bookService.getBookById(id);
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.getBookById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "book/edit";
+    }
+
+
+    @PostMapping("/edit")
+    public String editBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult, Model model) {
+        // Trường hợp có lỗi ràng buộc thì trả lại view edit
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/edit";
+        }
+        bookService.updateBook(book);
+        return "redirect:/books";
     }
 }
--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,6 +1,10 @@
 package com.example.demo.entity;
 
+import com.example.demo.validator.annotation.ValidCategoryId;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -10,13 +14,17 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotEmpty(message = "Tiêu đề không được để trống")
+    @Size(max = 50, min =1, message = "Tiêu đề phải ít hơn 50 ký tự")
     private String title;
 
     private String author;
 
+    @NotNull(message = "Price is required")
     private Double price;
+
     @ManyToOne
     @JoinColumn(name="category_id")
+    @ValidCategoryId
     private Category category;
 }
